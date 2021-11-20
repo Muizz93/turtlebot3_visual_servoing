bd5a2efc4691009bc06468f43118c315202ae52b
Muizz_laptop <abmuizz@gmail.com>
2021-11-20 22:17:04 +0100

    update controller

--- scripts/controller.py
+++ scripts/controller.py
@@ -3,32 +3,31 @@
 import rospy
 from std_msgs.msg import Float64MultiArray
 from geometry_msgs.msg import Twist
-import time
 import numpy as np
 from numpy.linalg import inv
 import time
 
 class Controller:
-    def __init__(self, p_gain = 0.01, i_gain = 0, d_gain = 0, current_time=None):
+    def __init__(self, p_gain = 0.01, i_gain = 0, d_gain = 0, current_time = None):
         self.kp, self.ki, self.kd = p_gain, i_gain, d_gain
-
         self.sample_time = 0.00
         self.current_time = current_time if current_time is not None else time.time()
         self.last_time = self.current_time
 
-        self.robot_pose_sub = rospy.Subscriber("robot_pose_raw", Float64MultiArray, self.robot_matrix)
+        self.robot_pose_sub = rospy.Subscriber("robot_pose_raw", Float64MultiArray, self.update_controller, queue_size = 1)
         self.parking_pose_sub = rospy.Subscriber("parking_pose", Float64MultiArray, self.parking_matrix)
         self.cmd_vel_pub = rospy.Publisher('/control/cmd_vel', Twist, queue_size = 1)
 
         rospy.on_shutdown(self.fnShutDown)
 
         self.clear()
-
-    def robot_matrix(self, robot_pose):
-        self.robot_hmat = np.array(robot_pose.data).reshape(robot_pose.layout.dim[1].size, robot_pose.layout.dim[2].size)
     
     def parking_matrix(self, parking_pose):
-        self.parking_hmat = np.array(parking_pose.data).reshape(parking_pose.layout.dim[1].size, parking_pose.layout.dim[2].size)
+        try:
+            self.parking_hmat = np.array(parking_pose.data).reshape(parking_pose.layout.dim[1].size, parking_pose.layout.dim[2].size)
+
+        except AttributeError:
+            pass
 
     def clear(self):
 
@@ -39,43 +38,44 @@ class Controller:
         self.PTerm = 0.0
         self.ITerm = 0.0
         self.DTerm = 0.0
-        self.err[1] = 0.0
-        self.err[0] = 0.0
+        self.err = [0.0, 0.0, 0.0]
 
         # Windup Guard
         self.int_error = 0.0
-        self.windup_guard = 20.0
+        self.windup_guard = 3.0
 
         self.u = 0.0
-        print('yes')
 
-
-    def compute_error(self):
+    def update_controller(self, robot_pose, current_time = None):
         try:
+            self.robot_hmat = np.array(robot_pose.data).reshape(robot_pose.layout.dim[1].size, robot_pose.layout.dim[2].size)
+
             delta_hmat = np.matmul(inv(self.robot_hmat), self.parking_hmat)
-            self.error = delta_hmat[0][3]
+            self.err[2] = delta_hmat[0][3]
+
+            self.current_time = current_time if current_time is not None else time.time()
+            delta_time = self.current_time - self.last_time
+
+            self.PTerm = self.kp * (self.err[2] - self.err[1])
+            self.ITerm = self.ki * self.err[2] * delta_time
+
+            if self.ITerm < -self.windup_guard:
+                self.ITerm = -self.windup_guard
+            elif self.ITerm > self.windup_guard:
+                self.ITerm = self.windup_guard
+
+            self.DTerm = self.kd * (self.err[2] - 2*self.err[1] + self.err[0])
+
+            self.last_time = self.current_time
+            self.err[1] = self.err[2]
+            self.err[0] = self.err[1]
+
+            self.u += self.PTerm + self.ITerm + self.DTerm
+            print(self.u)
 
         except AttributeError:
+            print('a')
             pass
-
-    def update_controller():
-
-        self.current_time = current_time if current_time is not None else time.time()
-        delta_time = self.current_time - self.last_time
-
-        self.err[2] = self.error
-
-        self.Pterm = self.kp * (self.err[2] - self.err[1])
-        self.Iterm = self.ki * self.err[2] * delta_time
-        self.Dterm = self.kd * (self.err[2] - 2*self.err[1] + self.err[0])
-
-        self.last_time = self.current_time
-        self.err[1] = self.err[2]
-        self.err[0] = self.err[1]
-
-        self.u += self.PTerm + self.ITerm + self.DTerm
-        print(self.u)
-
 
     def fnShutDown(self):
         rospy.loginfo("Shutting down. cmd_vel will be 0")
@@ -90,18 +90,11 @@ class Controller:
         self.cmd_vel_pub.publish(twist) 
     
 def main():
-    rospy.init_node('pose_estimation', anonymous=True)
+    rospy.init_node('controller', anonymous=True)
     rospy.loginfo("Initializing controller")
-    go_to_point = Controller(0.01)
-    rate = rospy.Rate(10)
-    while not rospy.is_shutdown:
-        go_to_point.compute_error()
-        a = go_to_point.update_controller()
-        print(a)
-        rate.sleep()
+    goto_point = Controller(0.1)
     
     rospy.spin()
-
 
 if __name__ == '__main__':
     try:
